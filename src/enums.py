a90ea2a8811fb55ceb8274ab518a8bffdb5fd3b7
flytie <lordly.powder@gmail.com>
2024-05-21 21:25:19 +0500

    fast commit

--- src/enums.py
+++ src/enums.py
@@ -2,6 +2,6 @@ from enum import StrEnum
 
 
 class ImgsDirsTemplates(StrEnum):
-    NO_MONEY = "../data/no_money.png"
-    MARKETPLACE = "../data/marketplace.png"
-    PURCHASE_CONFIRM = "../data/purchase_confirm.png"
+    NO_MONEY = "data/no_money.png"
+    MARKETPLACE = "data/marketplace.png"
+    PURCHASE_CONFIRM = "data/purchase_confirm.png"
--- src/game.py
+++ src/game.py
@@ -1,4 +1,10 @@
+import cv2
+
+
 class Game:    
     def __init__(self):
         pass
     
+class ImageWorker:    
+    def __init__(self):
+        pass
